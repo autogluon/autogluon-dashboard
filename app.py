8b08c2526a9d49d48d70cb8095ac52014eb22a79
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-14 14:56:12 -0700

    replace all strings with constants from config/

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from scripts.data import per_dataset_df, all_framework_df
 # Import helpers
 from scripts.widgets import create_selectwidget, create_togglewidget, create_numberwidget
 from scripts.plots import create_hvplot,create_table
-from scripts.constants import METRICS_TO_PLOT, GRAPH_TYPES, FRAMEWORK_LABEL, YAXIS_LABEL, DATASETS_LABEL, GRAPH_TYPE_STR
+from scripts.config.widgets_config import METRICS_TO_PLOT, GRAPH_TYPES
+from scripts.config.app_layout_config import APP_HEADER_BACKGROUND, APP_TITLE
+from scripts.config.plots_config import METRICS_PLOT_TITLE, TOP5_PERFORMERS_TITLE, AG_RANK_COUNTS_TITLE, FRAMEWORK_LABEL, YAXIS_LABEL, DATASETS_LABEL, GRAPH_TYPE_STR, RANK_LABEL, ERROR_COUNTS_TITLE, AUTOGLUON_RANK1_TITLE
 from scripts import utils
 
 #clean up framework names
@@ -33,51 +35,50 @@ graph_type2 = create_selectwidget(GRAPH_TYPE_STR, 'bar', GRAPH_TYPES)
 selected_dataset_df = utils.get_df_filter_by_dataset(per_dataset_idf, dataset_dropdown)
 df_ag_only = utils.get_df_filter_by_framework(per_dataset_df, 'AutoGluon')
 prop_ag_best = utils.get_proportion_framework_rank1(df_ag_only, per_dataset_df, len(dataset_list))
-autogluon_ranks = utils.get_col_metric_counts(df_ag_only, 'rank')
+autogluon_rank_counts = utils.get_col_metric_counts(df_ag_only, 'rank')
 per_dataset_top5 = utils.get_top5_performers(selected_dataset_df, 'rank')
 all_datasets_top5 = utils.get_top5_performers(all_framework_idf, 'rank')
 
 # Plots
-ihvplot_all = create_hvplot(idf=all_framework_idf, 
-                            title="Framework v/s Loss/Training Time/Inference Time/Best Diff ALL Datasets", 
+metrics_plot_all_datasets = create_hvplot(idf=all_framework_idf, 
+                            title=METRICS_PLOT_TITLE, 
                             x_axis='framework', 
                             y_axis=yaxis_widget, 
                             graph_type=graph_type, 
                             xlabel=FRAMEWORK_LABEL)
-ihvplot_dataset = create_hvplot(idf=selected_dataset_df, 
-                            title="Framework v/s Loss/Training Time/Inference Time/Best Diff per Dataset", 
+metrics_plot_per_datasets = create_hvplot(idf=selected_dataset_df, 
+                            title=METRICS_PLOT_TITLE, 
                             x_axis='framework', 
                             y_axis=yaxis_widget2, 
                             graph_type=graph_type2, 
                             xlabel=FRAMEWORK_LABEL)
-top5_all_datasets = create_table(all_datasets_top5, 
-                                 "Top 5 Performers (all datasets)", 
+top5frameworks_all_datasets = create_table(all_datasets_top5, 
+                                 TOP5_PERFORMERS_TITLE+" (all datasets)", 
                                  ['framework', 'rank'])
 top5frameworks_per_dataset = create_table(per_dataset_top5, 
-                                          "Top 5 Performers", 
+                                          TOP5_PERFORMERS_TITLE, 
                                           ['framework', 'rank'])
-ag_rank_hist = create_hvplot(idf=autogluon_ranks,
-                             title="Rank Frequency of AutoGluon", 
+ag_rank_hist = create_hvplot(idf=autogluon_rank_counts,
+                             title=AG_RANK_COUNTS_TITLE, 
                              rot=0,
-                             xlabel="Rank") 
+                             xlabel=RANK_LABEL) 
 framework_error = create_hvplot(idf=all_framework_idf,
-                             title="Framework v/s Error Counts",
+                             title=ERROR_COUNTS_TITLE,
                              x_axis='framework', 
                              y_axis='error_count',  
                              xlabel=FRAMEWORK_LABEL)
-ag_best = create_numberwidget('% 1st Rank for AutoGluon', round(prop_ag_best*100, 2), '{value}%')
+ag_best = create_numberwidget(AUTOGLUON_RANK1_TITLE, round(prop_ag_best*100, 2), '{value}%')
 
 # Layout using Template
 template = pn.template.FastListTemplate(
-    title='Interactive AutoGluon Dashboards', 
-    #sidebar=[yaxis, dataset_dropdown],
+    title=APP_TITLE, 
     main=[pn.Row('# All Datasets Comparison', 
                  pn.WidgetBox(
                      yaxis_widget, 
                      graph_type
                     ), 
-                    ihvplot_all.panel(), 
-                    top5_all_datasets
+                    metrics_plot_all_datasets.panel(), 
+                    top5frameworks_all_datasets
                 ), 
           pn.Row('# Per Dataset Comparison\n', 
                  pn.WidgetBox(
@@ -85,12 +86,12 @@ template = pn.template.FastListTemplate(
                      dataset_dropdown, 
                      graph_type2
                     ), 
-                    ihvplot_dataset.panel(), 
+                    metrics_plot_per_datasets.panel(), 
                     top5frameworks_per_dataset
                 ), 
           pn.Row('# Rank Comparisons', ag_best, ag_rank_hist),
           pn.Row('# Error Counts', framework_error)],
-    header_background="#88d8b0",
+    header_background=APP_HEADER_BACKGROUND,
 )
 
 template.servable()
