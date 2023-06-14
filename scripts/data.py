cf85c8bb49f7ba9e2bb08d51953e14e0b82650cd
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-14 16:03:09 -0700

    using autogluon.common.loaders.load_pd

--- scripts/data.py
+++ scripts/data.py
@@ -1,20 +1,12 @@
 import pandas as pd
+from autogluon.common.loaders import load_pd
 
 """
-import boto3
-
-client = boto3.client('s3')
-bucket_name = 'my_bucket'
-object_key = 'my_file.csv'
-csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
-body = csv_obj['Body']
-csv_string = body.read().decode('utf-8')
-df = pd.read_csv(StringIO(csv_string))
+df = load_pd.load(s3_PATH)
 """
 
 PER_DATASET_TEST_CSV= 'dev_data/all_data.csv'
 ALL_DATASETS_COMBINED_TEST_CSV = 'dev_data/autogluon.csv'
 
-
-per_dataset_df = pd.read_csv(PER_DATASET_TEST_CSV)
-all_framework_df = pd.read_csv(ALL_DATASETS_COMBINED_TEST_CSV)
+per_dataset_df = load_pd.load(PER_DATASET_TEST_CSV)
+all_framework_df = load_pd.load(ALL_DATASETS_COMBINED_TEST_CSV)
