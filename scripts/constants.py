2bb91e984ad27bc4c88ab98140bb0a4581023b89
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-14 13:20:21 -0700

    add config file for constants

---
+++ scripts/constants.py
@@ -0,0 +1,6 @@
+METRICS_TO_PLOT = ['loss_rescaled', 'time_train_s_rescaled','time_infer_s_rescaled', 'bestdiff']
+GRAPH_TYPES = ['line', 'bar','hist', 'scatter']
+FRAMEWORK_LABEL = 'Framework'
+YAXIS_LABEL = 'Y-Axis Metric'
+DATASETS_LABEL = 'Datasets'
+GRAPH_TYPE_STR = 'Graph Type'
