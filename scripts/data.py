a823fa683001ba80f494e190f7e933a512510317
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-16 12:05:40 -0700

    remove hardcoding of csv paths

--- scripts/data.py
+++ scripts/data.py
@@ -5,8 +5,7 @@ from autogluon.common.loaders import load_pd
 df = load_pd.load(s3_PATH)
 """
 
-PER_DATASET_TEST_CSV= 'dev_data/all_data.csv'
-ALL_DATASETS_COMBINED_TEST_CSV = 'dev_data/autogluon.csv'
-
-per_dataset_df = load_pd.load(PER_DATASET_TEST_CSV)
-all_framework_df = load_pd.load(ALL_DATASETS_COMBINED_TEST_CSV)
+def get_dataframes(csv1_path, csv2_path):
+    per_dataset_df = load_pd.load(csv1_path)
+    all_framework_df = load_pd.load(csv2_path)
+    return per_dataset_df, all_framework_df
