213364490f892e0ac2b2066c74e8e831a32816ad
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-16 14:10:37 -0700

    remove print + add new line

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,6 @@ import subprocess
 from multiprocessing import set_start_method
 
 if __name__ == '__main__':
-    print(sys.argv)
     csv1 = sys.argv[2]
     csv2 = sys.argv[4]
     subprocess.run(["panel", "serve", "app.py", "--autoreload", "--args", f"{csv1}", f"{csv2}"])
