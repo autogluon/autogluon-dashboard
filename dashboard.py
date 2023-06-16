7b20702132c11c4b143ba45c2e5dbeba25dba1d0
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-16 14:06:32 -0700

    wrapper file to get csv files from command line and launch panel subprocess

---
+++ dashboard.py
@@ -0,0 +1,10 @@
+import sys
+import subprocess
+from multiprocessing import set_start_method
+
+if __name__ == '__main__':
+    print(sys.argv)
+    csv1 = sys.argv[2]
+    csv2 = sys.argv[4]
+    set_start_method('fork')
+    subprocess.run(["panel", "serve", "app.py", "--autoreload", "--args", f"{csv1}", f"{csv2}"])
