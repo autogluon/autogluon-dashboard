ad2e5293e206e04d4c69c16ecfd35c9dfe6287ec
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-16 12:03:37 -0700

    rename config files

--- scripts/config/app_layout_config.py
+++
@@ -1,6 +0,0 @@
-APP_HEADER_BACKGROUND = "#88d8b0"
-APP_TITLE = "Interactive AutoGluon Dashboards"
-ALL_DATA_COMP = "# All Datasets Comparison"
-PER_DATA_COMP = "# Per Dataset Comparison"
-NO_RANK_COMP = "# Rank Comparisons"
-NO_ERROR_CNTS = "# Error Counts"
