750e75cd6037b9b34f586463c5f5bef130eb067a
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-19 10:28:38 -0700

    remove commented code

--- scripts/data.py
+++ scripts/data.py
@@ -1,10 +1,6 @@
 import pandas as pd
 from autogluon.common.loaders import load_pd
 
-"""
-df = load_pd.load(s3_PATH)
-"""
-
 def get_dataframes(csv1_path: str, csv2_path: str) -> tuple[pd.DataFrame, pd.DataFrame]:
     per_dataset_df = load_pd.load(csv1_path)
     all_framework_df = load_pd.load(csv2_path)
