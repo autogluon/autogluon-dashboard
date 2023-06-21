1072500867e4bc88575ef15911d8abd8ceeead39
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-21 15:53:42 -0700

    add black profile to isort

--- .github/workflow_scripts/lint_check.sh
+++ .github/workflow_scripts/lint_check.sh
@@ -7,4 +7,4 @@ source $(dirname "$0")/env_setup.sh
 setup_lint_env
 
 black --check --diff app/ scripts/ plotting/ unittests/
-isort --check --diff app/ scripts/ plotting/ unittests/
+isort --profile black --check --diff app/ scripts/ plotting/ unittests/ 
