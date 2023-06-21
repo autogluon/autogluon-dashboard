0fda45b157b5947f3b3bdd5ebea309cf90bd683c
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-20 17:33:55 -0700

    use pd.read_csv

--- scripts/data.py
+++ scripts/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from autogluon.common.loaders import load_pd
+#from autogluon.common.loaders import load_pd
 
 def get_dataframes(csv1_path: str, csv2_path: str) -> tuple[pd.DataFrame, pd.DataFrame]:
-    per_dataset_df = load_pd.load(csv1_path)
-    all_framework_df = load_pd.load(csv2_path)
+    per_dataset_df = pd.read_csv(csv1_path)
+    all_framework_df = pd.read_csv(csv2_path)
     return per_dataset_df, all_framework_df
