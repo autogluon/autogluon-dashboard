94286b4cd98702ff3d33ea557ab1d87e5b9c3c92
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-22 09:03:46 -0700

    src folder

--- .github/workflow_scripts/lint_check.sh
+++ .github/workflow_scripts/lint_check.sh
@@ -6,5 +6,5 @@ source $(dirname "$0")/env_setup.sh
 
 setup_lint_env
 
-black --check --diff app/ scripts/ plotting/ tests/
-isort --profile black --check --diff app/ scripts/ plotting/ tests/ 
+black --check --diff src/ tests/
+isort --profile black --check --diff src/ tests/ 
