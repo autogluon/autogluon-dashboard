d2fc551db3b9f9c369c5b368f27c60bbdc5c6b08
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-22 09:01:05 -0700

    move files to src folder

---
+++ src/plotting/all_plots.py
@@ -0,0 +1,110 @@
+from abc import abstractmethod
+from typing import Union
+
+import hvplot.pandas
+
+
+class Plot:
+    def __init__(
+        self,
+        plot_title: str,
+        dataset_to_plot: hvplot.Interactive,
+        plot_type: str,
+        x_axis: Union[str, list] = None,
+        y_axis: Union[str, list] = None,
+        graph_type: str = "bar",
+        xlabel: str = "",
+        ylabel: str = "",
+        label_rot: int = 90,
+        table_cols: list = [],
+    ) -> None:
+        self.plot_title = plot_title
+        self.df = dataset_to_plot
+        self.plot_x = x_axis
+        self.plot_y = y_axis
+        self.graph_type = graph_type
+        self.plot_x_label = xlabel
+        self.plot_y_label = ylabel
+        self.label_rot = label_rot
+        self.table_cols = table_cols
+
+        self.plot = self._create_table if plot_type == "table" else self._create_hvplot
+
+    @abstractmethod
+    def _preprocess(self, *args):
+        return
+
+    def _create_hvplot(
+        self,
+        color_scheme: list = ["#ff6f69", "#ffcc5c", "#88d8b0"],
+        line_width: Union[int, float] = 6,
+        height: Union[int, float] = 500,
+    ) -> hvplot.hvPlot:
+        """
+        Create a plot of a given type leveraging the hvplot library,
+        and using data from a pandas interactive dataframe.
+
+        Parameters
+        ----------
+        idf: Pandas dataframe,
+            Data to plot on.
+        x_axis: str or list, default=None,
+        Column name (as string) in dataframe or, list of values to plot on.
+        y_axis: str or list, default=None,
+            Column name (as string) in dataframe or, list of values to plot on.
+        graph_type: str, default='bar',
+            Type of plot. Examples: line, hist, bar, scatter
+        xlabel: str,
+            x-axis Label.
+        ylabel: str,
+            y-axis Label.
+        color_scheme: list, default=["#ff6f69", "#ffcc5c", "#88d8b0"],
+            Color scheme of plot.
+        line_width: int, default=6,
+            Width of line in plot.
+        height: int, default=500,
+            Height of plot frame.
+        rot: int, default=0,
+            Rotation amount for x-axis labels
+        """
+        if self.plot_x is None or self.plot_y is None:
+            return self.df.hvplot(
+                title=self.plot_title,
+                kind=self.graph_type,
+                color=color_scheme,
+                line_width=line_width,
+                height=height,
+                rot=self.label_rot,
+                xlabel=self.plot_x_label,
+                ylabel=self.plot_y_label,
+            )
+        return self.df.hvplot(
+            title=self.plot_title,
+            x=self.plot_x,
+            y=self.plot_y,
+            kind=self.graph_type,
+            color=color_scheme,
+            line_width=line_width,
+            height=height,
+            rot=self.label_rot,
+            xlabel=self.plot_x_label,
+        )
+
+    def _create_table(self, width: Union[int, float] = 800) -> hvplot.hvPlot.table:
+        """
+        Create a table leveraging the hvplot library,
+        and using data from a pandas interactive dataframe.
+
+        Parameters
+        ----------
+        idf: Pandas dataframe,
+            Data to create the table from.
+        title: str,
+            Title of the table.
+        columns: list,
+            List of strings of column names to create the table.
+            The strings should match column names in the provided idf.
+        width: int, default=800,
+            Width of the table.
+        """
+        return self.df.hvplot.table(title=self.plot_title, columns=self.table_cols, width=width)
---
+++ src/scripts/utils.py
@@ -0,0 +1,112 @@
+import pandas
+
+
+def get_sorted_names_from_col(df: pandas.DataFrame, col_name: str) -> list:
+    """
+    Get a sorted list of unique names in a given column.
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    col_name: str,
+        Name of Column to get names from.
+    """
+    return sorted(list(set(df[col_name])))
+
+
+def get_df_filter_by_dataset(df: pandas.DataFrame, dataset: str) -> pandas.DataFrame:
+    """
+    Get rows from dataframe pertaining to a particular dataset.
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    dataset: str,
+        Name of dataset to filter by.
+    """
+    return df[(df.dataset.isin([dataset]))]
+
+
+def get_df_filter_by_framework(df: pandas.DataFrame, framework: str) -> pandas.DataFrame:
+    """
+    Get rows from dataframe pertaining to a particular framework.
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    framework: str,
+        Name of dataset to filter by.
+    """
+    return df.loc[(df["framework"].isin([framework]))]
+
+
+def get_col_metric_counts(df: pandas.DataFrame, metric: str) -> pandas.Series:
+    """
+    Get counts of different unique values in a given column.
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    metric: str,
+        Name of Column to get counts from.
+    """
+    df_sorted = df.sort_values(by=[metric])
+    return df_sorted[metric].value_counts()[df_sorted[metric].unique()]
+
+
+def get_proportion_framework_rank1(
+    framework_df: pandas.DataFrame, datsets_df: pandas.DataFrame, total_runs: int
+) -> float:
+    """
+    Get proportion of #1 ranks for a given framework across all runs.
+
+    Parameters
+    ----------
+    framework_df: Pandas dataframe,
+        Dataframe for specific framework.
+    datsets_df: Pandas dataframe,
+        Dataframe for all datasets and frameworks.
+    total_runs: int,
+        Total #runs for a given framework = Total number of datasets.
+    """
+    return framework_df.loc[(datsets_df["rank"] == 1.0)].shape[0] / total_runs
+
+
+def get_top5_performers(df: pandas.DataFrame, metric: str) -> pandas.DataFrame:
+    """
+    Get top 5 performers for a given metric
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    metric: str,
+        Name of Column to get top 5 performers from.
+    """
+    return df.sort_values(metric).head()
+
+
+def get_name_before_first_underscore(df: pandas.DataFrame, col_name: str) -> pandas.DataFrame:
+    """
+    Get the part of the string before the first underscore.
+    Example: AutoGluonv0.1_gc8h8_2022 becomes AutoGluonv0.1
+
+    Parameters
+    ----------
+    df: Pandas dataframe,
+    col_name: str,
+        Name of Column to perform regex on.
+    """
+    return df[col_name].str.extract(r"^(.*?)(?:_|$)")[0]
+
+
+def clean_up_framework_names(df: pandas.DataFrame) -> list:
+    new_framework_names = get_name_before_first_underscore(df, "framework")
+    num_frameworks = len(set(new_framework_names))
+    # dummy framework replacement
+    for i in range(len(new_framework_names)):
+        new_framework_names[i] = (
+            "AutoGluon"
+            if i % num_frameworks == 0 or new_framework_names[i] == "AutoGluon"
+            else "AutoGluon v" + f"0.{i%num_frameworks}"
+        )
+    return new_framework_names
