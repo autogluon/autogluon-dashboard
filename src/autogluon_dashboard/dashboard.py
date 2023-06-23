d5f47bdc93b47e33720fcae2b060cd307d06aa8a
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-23 10:10:23 -0700

    remove hardcoding app.py path

--- src/autogluon_dashboard/dashboard.py
+++ src/autogluon_dashboard/dashboard.py
@@ -1,14 +1,17 @@
 import subprocess
 import sys
+import os
 
 if __name__ == "__main__":
     csv1 = sys.argv[2]
     csv2 = sys.argv[4]
+    wrapper_dir = os.path.dirname(__file__)
+    app_location = os.path.join(wrapper_dir, "app.py")
     subprocess.run(
         [
             "panel",
             "convert",
-            "src/autogluon_dashboard/app.py",
+            f"{app_location}",
             "--to",
             "pyodide-worker",
             "--out",
