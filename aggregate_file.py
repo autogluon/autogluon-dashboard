a18611e9fde28f02d60b80be0e4e1756552afac5
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-23 15:46:30 -0700

    added finding imports using ast lib and fixed trailing right paranthesis error

--- aggregate_file.py
+++ aggregate_file.py
@@ -1,69 +1,49 @@
+import ast
 import os
 import re
-import subprocess
-import sys
+from collections import namedtuple
 
 
-def get_imports(filepath):
-    """Extracts import statements and imported libraries from a Python file."""
-    with open(filepath, 'r') as file:
-        lines = file.readlines()
+def get_imports(path):
+    Import = namedtuple("Import", ["module", "name", "alias"])
+    with open(path) as fh:
+        root = ast.parse(fh.read(), path)
 
-    imports = []
-    imported_libraries = set()
-    for line in lines:
-        if line.startswith('import'):
-            import_line = line.rstrip()
-            libs = re.findall(r'[\w.]+', import_line.split('import')[-1])
-            imported_libraries.update(libs)
-            if ' as ' in import_line:
-                imports.append(import_line)
-            else:
-                match = re.match(r'import\s+(.+)\s+as\s+(\w+)', import_line)
-                if match:
-                    imports.append('import {} as {}'.format(match.group(1), match.group(2)))
-                else:
-                    modules = import_line.split('import')[1].split(',')
-                    modules = [module.strip() for module in modules]
-                    if len(modules) > 1:
-                        imports.append('import {}'.format(', '.join(modules)))
-                    else:
-                        imports.append('import {}'.format(modules[0]))
-        elif line.startswith('from'):
-            statement = line.rstrip()
-            match = re.match(r'from\s+((?:\w+\.)*\w+)\s+import\s+(.+)', statement)
-            if match:
-                package = match.group(1)
-                modules = match.group(2).split(',')
-                import_line = 'from {} import {}'.format(package, ', '.join(modules))
-                imports.append(import_line)
-                imported_libraries.add(package)
+    for node in ast.iter_child_nodes(root):
+        if isinstance(node, ast.Import):
+            module = []
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module.split(".")
+        else:
+            continue
 
-    imports = list(filter(lambda imp: not re.search(r'(?:\bas$)|(?:\bimport pn\b)|(?:\bimport pd\b)|(\bplotting\b)|(\bscripts\b)', imp), imports))
-    imported_libraries = list(filter(lambda imp: not re.search(r'(?:\bas$)|(?:\bpn\b)|(?:\bpd\b)|(\bplotting\b)|(\bscripts\b)', imp), imported_libraries))
-
-    return imports, imported_libraries
+        for n in node.names:
+            yield Import(module, n.name.split("."), n.asname)
 
 
 def extract_code(filepath):
     """Extracts code from a Python file excluding import statements."""
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         lines = file.readlines()
-
+    right_paran = False
     code_lines = []
     for line in lines:
-        if not (line.startswith('import') or line.startswith('from')):
-            if 'utils.' in line:
-                line = line.replace('utils.', '')
+        if not (line.startswith("import") or line.startswith("from")):
+            if "utils." in line:
+                line = line.replace("utils.", "")
+            if right_paran and ")" in line:
+                line = ""
+                right_paran = False
             code_lines.append(line)
+        elif "import (" in line:
+            right_paran = True
+    return "".join(code_lines)
 
-    return ''.join(code_lines)
 
 def create_merged_file(directory, output_file):
     """Creates a merged file with all code from the directory."""
     code = []
-    imports = []
-    imported_libraries = set()
+    imports = set()
 
     # Iterate over all files in the directory
     for filename in sorted(os.listdir(directory)):
@@ -74,35 +54,74 @@ def create_merged_file(directory, output_file):
             continue
 
         # Check if the file is the central file (app.py)
-        if filename == 'app.py':
-            # Extract import statements from app.py
+        if filename == "app.py":
             code.append(extract_code(filepath))
-            
+
+        elif filename == "dashboard.py":
+            continue
+
         else:
             # Extract code from other files
             code.append(extract_code(filepath))
-            files, libs = get_imports(filepath)
-            imports.extend(files)
-            imported_libraries.update(libs)
-    
-    additional_imports = [f"import {lib}" for lib in imported_libraries]
-    imports.extend(additional_imports)
+            imps = [import_lib for import_lib in get_imports(filepath)]
+            for imported in imps:
+                module = imported.module
+                alias = imported.alias
+                name = imported.name
+                # import statments with from keyword
+                if module:
+                    if len(module) == 1:
+                        if alias:
+                            imports.add(f"from {module[0]} import {name[0]} as {alias}")
+                        else:
+                            imports.add(f"from {module[0]} import {name[0]}")
 
+                    else:
+                        module_str = ""
+                        for sub_module in module:
+                            module_str += f"{sub_module}."
+                        module_str = module_str[:-1]
+                        if alias:
+                            imports.add(f"from {module_str} import {name[0]} as {alias}")
+                        else:
+                            imports.add(f"from {module_str} import {name[0]}")
+
+                # direct imports
+                else:
+                    if len(name) == 1:
+                        if alias:
+                            imports.add(f"import {name[0]} as {alias}")
+                        else:
+                            imports.add(f"import {name[0]}")
+
+                    else:
+                        name_str = ""
+                        for sub_module in name:
+                            name_str += f"{sub_module}."
+                        name_str = name_str[:-1]
+
+                        if alias:
+                            imports.add(f"import {name_str} as {alias}")
+                        else:
+                            imports.add(f"import {name_str}")
+
+    imports = list(filter(lambda imp: not re.search(r"plotting|scripts|utils|Plot", imp), imports))
     # Combine all code and imports
-    merged_code = '\n'.join(imports) + '\n\n' + '\n'.join(code)
+    merged_code = "\n".join(imports) + "\n\n" + "\n".join(code)
 
     # Write the merged code to the output file
-    with open(output_file, 'a') as file:
+    with open(output_file, "a") as file:
         file.write(merged_code)
 
-if __name__ == '__main__':
-    # Specify the directory containing your files and the desired output file name
-    out_file = 'out.py'
+
+if __name__ == "__main__":
+    # Specify the directory containing the files and the desired output file name
+    out_file = "out.py"
     if os.path.exists(out_file):
         os.remove(out_file)
     else:
-        open(out_file, 'x')
-    create_merged_file('src/scripts/constants', 'out.py')
-    create_merged_file('src/scripts', 'out.py')
-    create_merged_file('src/plotting', 'out.py')
-    create_merged_file('src/app', 'out.py')
+        open(out_file, "x")
+    create_merged_file("src/autogluon_dashboard/scripts/constants", "out.py")
+    create_merged_file("src/autogluon_dashboard/scripts", "out.py")
+    create_merged_file("src/autogluon_dashboard/plotting", "out.py")
+    create_merged_file("src/autogluon_dashboard", "out.py")
