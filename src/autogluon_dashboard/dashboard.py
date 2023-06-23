7a96a041a7ff07e705fb4aac41b2461e011efd34
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-23 09:58:57 -0700

    remove commented code and ran black

--- src/autogluon_dashboard/dashboard.py
+++ src/autogluon_dashboard/dashboard.py
@@ -1,12 +1,21 @@
 import subprocess
 import sys
 
-from autogluon.common.loaders import load_pd
-
 if __name__ == "__main__":
     csv1 = sys.argv[2]
     csv2 = sys.argv[4]
-    # subprocess.run(["panel", "serve", "src/autogluon_dashboard/app.py", "--autoreload", "--args", f"{csv1}", f"{csv2}"])
     subprocess.run(
-        ["panel", "convert", "src/autogluon_dashboard/app.py", "--to", "pyodide-worker", "--out", "docs/app"]
+        [
+            "panel",
+            "convert",
+            "src/autogluon_dashboard/app.py",
+            "--to",
+            "pyodide-worker",
+            "--out",
+            "docs/app",
+            "--requirements",
+            "pandas",
+            "holoviews",
+            "hvplot",
+        ]
     )
