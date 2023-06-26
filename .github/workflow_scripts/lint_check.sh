6ee71e4d33a96e31641e02f13c471ea80f3ce4b0
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-26 10:50:19 -0700

    ignore out_files from aggregate

--- .github/workflow_scripts/lint_check.sh
+++ .github/workflow_scripts/lint_check.sh
@@ -6,5 +6,5 @@ source $(dirname "$0")/env_setup.sh
 
 setup_lint_env
 
-black --check --diff src/ tests/
-isort --profile black --check --diff src/ tests/ 
+black --check --diff src/ tests/ --extend-exclude tests/unittests/out_file
+isort --profile black --check --diff src/ tests/ --skip-glob tests/unittests/out_file
