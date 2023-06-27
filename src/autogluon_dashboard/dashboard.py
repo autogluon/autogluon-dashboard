d5db7a4063de4be74cc8e2ad39117e9bd4775587
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-27 10:01:07 -0700

    fix s3 uploading

--- src/autogluon_dashboard/dashboard.py
+++ src/autogluon_dashboard/dashboard.py
@@ -3,27 +3,28 @@ import subprocess
 import sys
 
 import boto3
+import botocore
 
-s3 = boto3.client("s3")
 BUCKET = "dashboard-test-yash"
 s3_url = "https://dashboard-test-yash.s3.us-west-2.amazonaws.com/"
 
 
-def upload_to_s3(file_name: str, object_name: str):
+def upload_to_s3(s3_client: botocore.client, file_name: str, object_name: str):
     """
     Uploads a file from local filesystem to a specified S3 bucket
 
     Parameters
     ----------
+    s3_client: botocore.client.S3,
+        S3 client to interact with bucket
     file_name: str,
         Name of local file to upload to S3
     object_name: str,
         Name of object to store file contents in S3 bucket
     """
-    global s3
     global BUCKET
     with open(file_name, "rb") as f:
-        s3.upload_fileobj(f, BUCKET, object_name)
+        s3_client.upload_fileobj(f, BUCKET, object_name)
 
 
 def run_dashboard():
@@ -32,10 +33,11 @@ def run_dashboard():
     per_dataset_s3_loc = "dev_data/all_data.csv"
     aggregated_s3_loc = "dev_data/autogluon.csv"
     global s3_url
+    s3_client = boto3.client("s3")
     os.environ["PER_DATASET_PATH"] = s3_url + per_dataset_s3_loc
     os.environ["AGG_DATASET_PATH"] = s3_url + aggregated_s3_loc
-    upload_to_s3(per_dataset_csv_path, per_dataset_s3_loc)
-    upload_to_s3(aggregated_csv_path, aggregated_s3_loc)
+    upload_to_s3(s3_client, per_dataset_csv_path, per_dataset_s3_loc)
+    upload_to_s3(s3_client, aggregated_csv_path, aggregated_s3_loc)
     wrapper_dir = os.path.dirname(__file__)
     app_location = os.path.join(wrapper_dir, "app.py")
     subprocess.run(
@@ -53,8 +55,8 @@ def run_dashboard():
             "hvplot",
         ]
     )
-    upload_to_s3("web_files/app.html", "app.html")
-    upload_to_s3("web_files/app.html", "app.js")
+    upload_to_s3(s3_client, "web_files/app.html", "app.html")
+    upload_to_s3(s3_client, "web_files/app.html", "app.js")
 
 
 if __name__ == "__main__":
