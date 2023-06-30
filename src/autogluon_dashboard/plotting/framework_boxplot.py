48574be71c9538c1c4340a7df4ee59b4006990f0
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-06-30 11:28:52 -0700

    framework boxplot class

---
+++ src/autogluon_dashboard/plotting/framework_boxplot.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+import hvplot
+
+from .all_plots import Plot
+
+
+class FrameworkBoxPlot(Plot):
+    def __init__(
+        self,
+        plot_title: str,
+        dataset_to_plot: hvplot.Interactive,
+        plot_type: str = "hvplot",
+        x_axis: Union[str, list] = None,
+        y_axis: Union[str, list] = None,
+        graph_type: str = "box",
+        xlabel: str = "",
+        ylabel: str = "",
+        label_rot: int = 90,
+        table_cols: list = ...,
+    ) -> None:
+        super(FrameworkBoxPlot, self).__init__(
+            plot_title,
+            dataset_to_plot,
+            plot_type,
+            x_axis,
+            y_axis,
+            graph_type,
+            xlabel,
+            ylabel,
+            label_rot,
+            table_cols,
+        )
+        self.plot = self._box_plot
+
+    def _preprocess(*args) -> None:
+        return Plot._preprocess(*args)
+
+    def _box_plot(self) -> hvplot.hvPlot.box:
+        return self.df.hvplot.box(self.plot_y, by="framework", rot=self.label_rot)
