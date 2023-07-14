a77f83409bb6dbf9dda7981095a700230f7e4808
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-14 14:57:42 -0700

    reformat constants file

--- src/autogluon_dashboard/scripts/constants/df_constants.py
+++ src/autogluon_dashboard/scripts/constants/df_constants.py
@@ -9,28 +9,20 @@ TIME_TRAIN_S = "time_train_s"
 TIME_INFER_S = "time_infer_s"
 TIME_TRAIN_S_RESCALED = "time_train_s_rescaled"
 TIME_INFER_S_RESCALED = "time_infer_s_rescaled"
-
-DATASET = "dataset"
-FOLD = "fold"
-RANK = "rank"
+LOSS_RESCALED = "loss_rescaled"
+BESTDIFF = "bestdiff"
 
 METRIC_ERROR = "metric_error"
-METRIC_SCORE = "metric_score"
 METRIC = "metric"
 PROBLEM_TYPE = "problem_type"
 
-NUM_CLASSES = "num_classes"
-NUM_COLS = "num_cols"
-NUM_ROWS = "num_rows"
-DATETIME = "datetime"
+DATASET = "dataset"
+FOLD = "fold"
 
+RANK = "rank"
+WINRATE = "winrate"
 ERROR_COUNT = "error_count"
 RANK_1 = "rank=1_count"
 
-BESTDIFF = "bestdiff"
-
-LOSS_RESCALED = "loss_rescaled"
-
 UTC = "utc"
-
-WINRATE = "winrate"
+DATETIME = "datetime"
