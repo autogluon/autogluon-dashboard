0192de5efa6b8353f903c6694ea05c3677c84e56
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 12:15:41 -0700

    docstring for error_datasets class

--- src/autogluon_dashboard/plotting/errored_datasets.py
+++ src/autogluon_dashboard/plotting/errored_datasets.py
@@ -8,8 +8,45 @@ from autogluon_dashboard.scripts.constants.df_constants import DATASET
 from ..scripts.utils import get_df_filter_by_framework, get_sorted_names_from_col
 from .all_plots import Plot
 
+ERRORED_DATASETS = "Errored Datasets"
+
 
 class ErroredDatasets(Plot):
+    """
+    This class is used to create a table of datasets that errored out during a benchmark run for a given framework.
+
+    Attributes
+    ----------
+    plot_title: str,
+        title of the plot on the dashboard website
+    dataset_to_plot: hvplot.Interactive,
+        interactive pandas dataframe that is used to create the plot
+    plot_type: str,
+        type of hvplot. hvplot, table, pareto
+    x_axis: Optional[Union[str, List[str]]],
+        values to plot on x-axis
+    y_axis: Optional[Union[str, List[str]]],
+        values to plot on y-axis
+    graph_type: str,
+        type of graph. examples: bar, line, hist
+    xlabel: str,
+        label of x-axis
+    ylabel: str,
+        label of y-axis
+    label_rot: int,
+        rotation value of labels on axes
+    table_cols: list,
+        list of columns to use for table plots
+    table_width: Optional[int],
+        width of table
+
+    Methods
+    -------
+    _preprocess():
+        inherited from parent `Plot` class
+        returns a table containing the datasets that errored out for a provided framework
+    """
+
     def __init__(
         self,
         plot_title: str,
@@ -35,7 +72,7 @@ class ErroredDatasets(Plot):
             xlabel,
             ylabel,
             label_rot,
-            ["Errored Datasets"],
+            [ERRORED_DATASETS],
         )
 
     def _preprocess(self, df, framework, **kwargs) -> pandas.DataFrame:
@@ -43,4 +80,4 @@ class ErroredDatasets(Plot):
         df_filtered_by_framework = get_df_filter_by_framework(df, framework)
         datasets = df_filtered_by_framework.dataset.values
         errored_datasets = list(set(dataset_list).difference(datasets))
-        return pandas.DataFrame({"Errored Datasets": errored_datasets})
+        return pandas.DataFrame({ERRORED_DATASETS: errored_datasets})
