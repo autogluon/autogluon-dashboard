fc889b29329b86a6b6bc194187d22f527a06bb25
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 15:44:50 -0700

    docstring for pareto class

--- src/autogluon_dashboard/plotting/pareto_front.py
+++ src/autogluon_dashboard/plotting/pareto_front.py
@@ -7,6 +7,36 @@ from .all_plots import Plot
 
 
 class ParetoFront(Plot):
+    """
+    This class is used to create a pareto frontier plot of inference time v/s winrate, by framework.
+
+    Attributes
+    ----------
+    plot_title: str,
+        title of the plot on the dashboard website
+    df_process: hvplot.Interactive,
+        interactive pandas dataframe that is used to create the plot
+        this should be the dataset of all frameworks aggregated across all datasets
+    plot_type: str,
+        type of hvplot. hvplot, table, pareto
+    x_axis: Optional[Union[str, List[str]]],
+        values to plot on x-axis
+    y_axis: Optional[Union[str, List[str]]],
+        values to plot on y-axis
+    xlabel: str,
+        label of x-axis
+    ylabel: str,
+        label of y-axis
+    label_rot: int,
+        rotation value of labels on axes
+
+    Usage
+    ------
+    >>> pareto_front = ParetoFront("pareto plot", all_frameworks_df, "pareto", x_axis=TIME_INFER_S_RESCALED, y_axis=WINRATE)
+
+    You can now call the `.plot()` method on this object to render the table as a Panel object on the dashboard website.
+    
+    """
     def __init__(
         self,
         plot_title: str,
@@ -14,11 +44,9 @@ class ParetoFront(Plot):
         plot_type: str,
         x_axis: Optional[Union[str, List[str]]] = None,
         y_axis: Optional[Union[str, List[str]]] = None,
-        graph_type: str = "bar",
         xlabel: str = "",
         ylabel: str = "",
         label_rot: int = 90,
-        table_cols: list = [],
     ) -> None:
         super().__init__(
             plot_title,
@@ -26,9 +54,7 @@ class ParetoFront(Plot):
             plot_type,
             x_axis,
             y_axis,
-            graph_type,
             xlabel,
             ylabel,
             label_rot,
-            table_cols,
         )
