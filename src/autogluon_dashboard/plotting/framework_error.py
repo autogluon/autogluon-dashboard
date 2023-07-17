b66a1861cc5c55a1159ac5c1b8744eda9ae64b04
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 12:30:11 -0700

    docstring for framework error

--- src/autogluon_dashboard/plotting/framework_error.py
+++ src/autogluon_dashboard/plotting/framework_error.py
@@ -6,6 +6,46 @@ from .all_plots import Plot
 
 
 class FrameworkError(Plot):
+    """
+    This class is used to create a table of datasets that errored out during a benchmark run for a given framework.
+
+    Attributes
+    ----------
+    plot_title: str,
+        title of the plot on the dashboard website
+    dataset_to_plot: hvplot.Interactive,
+        interactive pandas dataframe that is used to create the plot
+        this dataframe will first go through the preprocess method
+    plot_type: str,
+        type of hvplot. hvplot, table, pareto
+    framework: str,
+        framework to query for errored datasets
+    x_axis: Optional[Union[str, List[str]]],
+        values to plot on x-axis
+    y_axis: Optional[Union[str, List[str]]],
+        values to plot on y-axis
+    graph_type: str,
+        type of graph. examples: bar, line, hist
+    xlabel: str,
+        label of x-axis
+    ylabel: str,
+        label of y-axis
+    label_rot: int,
+        rotation value of labels on axes
+    
+    Usage
+    ------
+    >>> framework_error = FrameworkError(
+            "framework error",
+            benchmark_df,
+            "hvplot",
+            x_axis="framework",
+            y_axis="error_count",
+        )
+    
+    You can now call the `.plot()` method on this object to render the plot as a Panel object on the dashboard website.
+    
+    """
     def __init__(
         self,
         plot_title: str,
@@ -17,7 +57,6 @@ class FrameworkError(Plot):
         xlabel: str = "",
         ylabel: str = "",
         label_rot: int = 90,
-        table_cols: list = [],
     ) -> None:
         super().__init__(
             plot_title,
@@ -29,5 +68,4 @@ class FrameworkError(Plot):
             xlabel,
             ylabel,
             label_rot,
-            table_cols,
         )
