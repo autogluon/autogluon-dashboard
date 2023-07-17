416ea8d507f4f06dcce312f2c08cc653e7aee0dd
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 15:47:13 -0700

    add abstractmethod decorator to create widget

--- src/autogluon_dashboard/scripts/widgets/widget.py
+++ src/autogluon_dashboard/scripts/widgets/widget.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+
+
 class Widget:
     """
     This parent class is used to define any Panel widgets that are used on the dashboard website.
@@ -7,5 +10,6 @@ class Widget:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+    @abstractmethod
     def create_widget(self):
-        return
+        raise NotImplementedError
