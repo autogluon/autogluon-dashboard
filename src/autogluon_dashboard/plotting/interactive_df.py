96ec67786287e581ae1668e1d8e9985780d9e5bf
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 15:33:41 -0700

    docstring for interactive dataframe class

--- src/autogluon_dashboard/plotting/interactive_df.py
+++ src/autogluon_dashboard/plotting/interactive_df.py
@@ -8,6 +8,39 @@ from .all_plots import Plot
 
 
 class InteractiveDataframe(Plot):
+    """
+    This class is used to create an ineractive python dataframe, displayed as a table
+    This allows the user to view the source data (csv files) being used to compose the plots
+
+    Attributes
+    ----------
+    df_process: hvplot.Interactive,
+        interactive pandas dataframe that is used to create the plot
+        this dataframe will first go through the preprocess method
+    framework: str,
+        if desired, what framework the table should be filered by
+        default = All Frameworks
+    width: int,
+        width of output table
+    dataset: Optional[str],
+        if desired, what dataset the table should be filered by
+
+    Methods
+    -------
+    _preprocess():
+        inherited from parent `Plot` class
+        returns the dataframe filtered by provided framework and dataset
+    
+    get_interactive_df():
+        returns an interactive table of the pandas dataframe
+
+    Usage
+    ------
+    >>> interactive_df = InteractiveDataframe(benchmark_df, "AutoGluon", width=3000, dataset="Dataset A")
+    
+    You can now call the `.get_interactive_df()` method on this object to render the table as a Panel object on the dashboard website.
+    
+    """
     def __init__(
         self, df_process: hvplot.Interactive, framework: str, width: int, dataset: Optional[str] = None
     ) -> None:
@@ -28,9 +61,3 @@ class InteractiveDataframe(Plot):
         if "All Frameworks" in framework:
             return df
         return get_df_filter_by_framework(df, framework)
-
-    def _process_df(self, df_process, dataset) -> pandas.DataFrame:
-        print(dataset)
-        if dataset:
-            df_process = get_df_filter_by_dataset(df_process, dataset)
-        return df_process
