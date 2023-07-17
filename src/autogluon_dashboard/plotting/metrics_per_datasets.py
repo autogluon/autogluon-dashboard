36813df33de49d3efd0d2957dade02ff507d72c3
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-17 15:44:32 -0700

    docstring for metrics_per_datasets class

--- src/autogluon_dashboard/plotting/metrics_per_datasets.py
+++ src/autogluon_dashboard/plotting/metrics_per_datasets.py
@@ -8,6 +8,55 @@ from .all_plots import Plot
 
 
 class MetricsPlotPerDataset(Plot):
+    """
+    This class is used to create a plot of frameworks v/s desired metrics (for a particular dataset)
+
+    Attributes
+    ----------
+    plot_title: str,
+        title of the plot on the dashboard website
+    dataset_to_plot: hvplot.Interactive,
+        interactive pandas dataframe that is used to create the plot
+        this should be the dataset of all frameworks and all datasets
+    plot_type: str,
+        type of hvplot. hvplot, table, pareto
+    dataset: str,
+        dataset to filter table by
+    x_axis: Optional[Union[str, List[str]]],
+        values to plot on x-axis
+    y_axis: Optional[Union[str, List[str]]],
+        values to plot on y-axis
+    graph_type: str,
+        type of graph. examples: bar, line, hist
+    xlabel: str,
+        label of x-axis
+    ylabel: str,
+        label of y-axis
+    label_rot: int,
+        rotation value of labels on axes
+
+    Methods
+    -------
+    _preprocess():
+        inherited from parent `Plot` class
+        returns a filtered dataframe by provided dataset
+
+    Usage
+    ------
+    >>> metrics_plot_all_datasets = MetricsPlotPerDataset(
+            METRICS_PLOT_TITLE,
+            benchmark_df,
+            "hvplot",
+            dataset="Dataset A",
+            x_axis="framework",
+            y_axis="loss_rescaled",
+            graph_type=graph_dropdown,
+            xlabel=FRAMEWORK_LABELS,
+        )
+    
+    You can now call the `.plot()` method on this object to render the table as a Panel object on the dashboard website.
+    
+    """
     def __init__(
         self,
         plot_title: str,
@@ -20,7 +69,6 @@ class MetricsPlotPerDataset(Plot):
         xlabel: str = "",
         ylabel: str = "",
         label_rot: int = 90,
-        table_cols: list = [],
     ) -> None:
         dataset_to_plot = self._preprocess(df=df_process, dataset_name=dataset)
         super().__init__(
@@ -33,7 +81,6 @@ class MetricsPlotPerDataset(Plot):
             xlabel,
             ylabel,
             label_rot,
-            table_cols,
         )
 
     def _preprocess(self, df, dataset_name, **kwargs) -> pandas.DataFrame:
