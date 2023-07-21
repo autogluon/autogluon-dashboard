0b8d443f0b723b82f8c1b085ef617b2512309b53
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-20 17:23:57 -0700

    make test coverage pass

--- .github/workflow_scripts/test_dashboard.sh
+++ .github/workflow_scripts/test_dashboard.sh
@@ -5,4 +5,4 @@ source $(dirname "$0")/env_setup.sh
 
 install_dashboard_test
 
-python3 -m pytest --junitxml=results.xml tests/unittests/ --cov-report json:tests/unittests/coverage/coverage.json
+python3 -m pytest --junitxml=results.xml tests/unittests/ --cov-report json:./coverage.json
