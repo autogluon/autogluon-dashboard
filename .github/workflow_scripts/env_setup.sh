1f261ede0cce978f6ddf9a1bd82eca984d91a099
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-20 17:03:54 -0700

    unittest coverage

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -7,4 +7,5 @@ function setup_lint_env {
 function install_dashboard_test {
     python3 -m pip install --upgrade -e "./[tests]"
     python3 -m pip install pytest
+    python3 -m pip install coverage-threshold
 }
