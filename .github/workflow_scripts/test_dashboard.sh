9be579fdcb10d559d7ae84f13851028ef0110785
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-20 17:36:02 -0700

    fix missing json file issue

--- .github/workflow_scripts/test_dashboard.sh
+++ .github/workflow_scripts/test_dashboard.sh
@@ -5,4 +5,5 @@ source $(dirname "$0")/env_setup.sh
 
 install_dashboard_test
 
-python3 -m pytest --junitxml=results.xml tests/unittests/ --cov-report json:./coverage.json
+python3 -m pytest --junitxml=results.xml tests/unittests/ --ignore=tests/unittests/coverage/ --cov-report json
+python3 -m pytest tests/unittests/coverage/test_coverage.py
