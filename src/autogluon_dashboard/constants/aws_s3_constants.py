f6a84525cb16f3d78a87bb69bb70083d7fc83027
Shreyash Iyengar <54525027+shreyash2106@users.noreply.github.com>
2023-08-07 15:22:33 -0700

    Update S3 constants (#40)
    * change s3 default to autogluon interns bucket

--- src/autogluon_dashboard/constants/aws_s3_constants.py
+++ src/autogluon_dashboard/constants/aws_s3_constants.py
@@ -1,5 +1,5 @@
-S3_REGION = "us-west-2"
-DEFAULT_BUCKET_NAME = "dashboard-test-yash"  # TODO: Change default bucket name
+S3_REGION = "us-east-2"
+DEFAULT_BUCKET_NAME = "ag-dashboard-test"  # TODO: Change default bucket name
 CSV_FILES_DIR = "dev_data/"
 PER_DATASET_DEFAULT_CSV_PATH = (
     "https://d24iwcyhf6yavw.cloudfront.net/dev_data/all_data.csv"  # TODO: Change default csv path
