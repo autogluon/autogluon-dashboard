36b45114b699a6c3383ed480af71776c1c989f29
Shreyash Sridhar Iyengar <54525027+shreyash2106@users.noreply.github.com>
2023-08-10 16:18:40 -0700

    Make s3 bucket name and s3 region required parameters in dashboard wrapper. Make hardware_metrics optional. Fix custom csv path issue when fetching from s3 (#46)
    * Make s3 bucket name and s3 region required parameters in dashboard wrapper. Make hardware_metrics optional. Fix custom csv path issue when fetching from s3
    
    * remove DEFAULT in csv path
    
    * formatting
    
    * change csv defaults
    
    * ignore csv paths file + remove print statement
    
    * address PR comments
    
    * fix unittest
    
    * ignore csv_paths file to prevent changing of defaults

---
+++ src/autogluon_dashboard/constants/csv_paths.py
@@ -0,0 +1,3 @@
+PER_DATASET_CSV_PATH = "https://d24iwcyhf6yavw.cloudfront.net/dev_data/all_data.csv"
+AGG_FRAMEWORK_CSV_PATH = "https://d24iwcyhf6yavw.cloudfront.net/dev_data/autogluon.csv"
+HARDWARE_METRICS_CSV_PATH = "https://d24iwcyhf6yavw.cloudfront.net/dev_data/hardware_metrics.csv"
