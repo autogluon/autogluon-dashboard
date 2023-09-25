22a9ad739afb6b1d7e26b32abfea8bf808cabb7c
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-09-25 21:53:50 +0000

    Minor fix - keeping framework names as is

--- src/autogluon_dashboard/utils/dataset_utils.py
+++ src/autogluon_dashboard/utils/dataset_utils.py
@@ -98,11 +98,7 @@ def get_name_before_first_underscore(df: pandas.DataFrame, col_name: str) -> pan
     col_name: str,
         Name of Column to perform filtering on.
     """
-    # If not triggered with benchmark tests, then rename framework to AutoGluon
-    if not df[col_name].str.contains("master", case=False).any():
-        return df[col_name].str.extract(r"^(.*?)(?:_|$)")[0]
-    else:
-        return df[col_name]
+    return df[col_name]
 
 
 def clean_up_framework_names(df: pandas.DataFrame, dummy: bool = False) -> list:
