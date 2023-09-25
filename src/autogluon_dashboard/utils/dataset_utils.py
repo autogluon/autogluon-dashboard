0ae28c32015c4f2ef3eb36cd11fb4c9730bf428b
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-09-25 20:29:56 +0000

    Lint fix

--- src/autogluon_dashboard/utils/dataset_utils.py
+++ src/autogluon_dashboard/utils/dataset_utils.py
@@ -99,7 +99,7 @@ def get_name_before_first_underscore(df: pandas.DataFrame, col_name: str) -> pan
         Name of Column to perform filtering on.
     """
     # If not triggered with benchmark tests, then rename framework to AutoGluon
-    if not df[col_name].str.contains('master', case=False).any():
+    if not df[col_name].str.contains("master", case=False).any():
         return df[col_name].str.extract(r"^(.*?)(?:_|$)")[0]
     else:
         return df[col_name]
